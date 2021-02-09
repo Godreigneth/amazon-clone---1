056c587205b96eb3bbb644dbe2e86d455b54a506
Godreigneth Levi <godreignethlevidibia@yahoo.com>
2021-02-09 13:29:56 +0100

    Creasted firebase.js file under src and implemented Firebase to my Amazon clone

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyD5TAAYJNculyzkx39W-4vc-EsOWYk3OdY",
+  authDomain: "clone-78c5f.firebaseapp.com",
+  projectId: "clone-78c5f",
+  storageBucket: "clone-78c5f.appspot.com",
+  messagingSenderId: "243304419722",
+  appId: "1:243304419722:web:d2dbe0ab9e8d9636e87873",
+  measurementId: "G-3WTCBP60BN",
+};
