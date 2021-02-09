fe060279081a66ecc13a6978da705e70000d3a9c
Godreigneth Levi <godreignethlevidibia@yahoo.com>
2021-02-09 14:56:38 +0100

    Imported the Header component, and added it to the return function

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
 import './App.css';
 
+import Header from './components/Header'
+
 function App() {
   return (
-    <div className="App">
-     
+    //Following BEM Convention
+    <div className="app">
+      <Header />
     </div>
   );
 }
